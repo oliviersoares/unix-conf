ef5f3c87403cc4603e35b7dcfcb2e51810262dea
oliv <oliv@oliv.xxx>
2020-03-06 17:34:13 -0800

    ._

--- bin/sysclean.sh
+++ bin/sysclean.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Darwin" ]; then
-  find ~/ -type f \( -name ".DS_Store" -o name "._*" -o -name ".localized" \) -exec rm -f {} \;
+  find ~/ -type f \( -name ".DS_Store" -o -name "._*" -o -name ".localized" \) -exec rm -f {} \;
   rm -rf ~/Library/Logs/*
   qlmanage -r cache
 fi
